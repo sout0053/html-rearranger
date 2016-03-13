4b15c9281e0ca34906558a373921a2961b96fd71
sout0053 <maxwell.southwood@outlook.com>
2016-03-12 21:54:24 -0500

    Changed variable names

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 var $body = $('body');
-var $headerB = $('<h2>');
-var $ulB = $('<ul>');
-var $headerA = $('<h2>');
-var $ulA = $('<ul>');
+var $header2 = $('<h2>');
+var $ul2 = $('<ul>');
+var $header1 = $('<h2>');
+var $ul1 = $('<ul>');
 var $ul = $('ul');
 var $li = $('ul li');
 
-$body.append($headerB);
-$headerB.html('Below ground veggies');
-$body.append($ulB);
-$body.append($headerA);
-$headerA.html('Above ground veggies');
-$body.append($ulA);
+$body.append($header2);
+$header2.html('Below ground veggies');
+$body.append($ul2);
+$body.append($header1);
+$header1.html('Above ground veggies');
+$body.append($ul1);
 
 $li.each(function () {
   if ($(this).hasClass('below')) {
-    $ulB.append($(this));
+    $ul2.append($(this));
   } else {
-    $ulA.append($(this));
+    $ul1.append($(this));
   }
 });
